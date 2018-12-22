f0fdb8ab205158b07c18dada979c4eb2c5246f08
myqzf <qzf98763@163.com>
2018-12-22 16:07:40 +0800

    初始化

---
+++ src/main/java/com/qzf/App.java
@@ -0,0 +1,13 @@
+package com.qzf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
